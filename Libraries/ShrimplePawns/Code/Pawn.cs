9ec7ebb88b0c2eb5f529f27a568ecff754997d6b
Matthew Zegar <matthewzegar@gmail.com>
2024-11-16 21:19:48 -0800

    Let the user assign owner themselves

--- Libraries/ShrimplePawns/Code/Pawn.cs
+++ Libraries/ShrimplePawns/Code/Pawn.cs
@@ -5,40 +5,12 @@ namespace ShrimplePawns;
 /// </summary>
 public abstract class Pawn : Component
 {
-	[HostSync]
-	protected Client Owner { get; set; }
-
-	/// <summary>
-	/// Get the owner only if it is valid.
-	/// </summary>
-	public bool TryGetOwner<T>( out T owner ) where T : Client
-	{
-		owner = GetOwner<T>();
-		return owner.IsValid();
-	}
-
-	/// <summary>
-	/// Get the owner.
-	/// </summary>
-	public T GetOwner<T>() where T : Client
-	{
-		return Owner as T;
-	}
-
-	/// <summary>
-	/// Get the owner.
-	/// </summary>
-	public Client GetOwner()
-	{
-		return Owner;
-	}
-
 	/// <summary>
 	/// Called when the pawn has been assigned.
 	/// </summary>
 	public virtual void OnAssign( Client client )
 	{
-		Owner = client;
+
 	}
 
 	/// <summary>
