0afad31e518c1d5efeb912315de869c3ef704c4a
Matthew Zegar <matthewzegar@gmail.com>
2025-02-15 08:17:42 -0800

    Use primary constructor for pawn attribute

--- Libraries/ShrimplePawns/Code/PawnAttribute.cs
+++ Libraries/ShrimplePawns/Code/PawnAttribute.cs
@@ -5,14 +5,9 @@ namespace ShrimplePawns;
 /// <summary>
 /// This attribute should be given to any class that derives Pawn.cs
 /// </summary>
+/// <param name="prefabPath">The path to the prefab that contains the derived pawn component.</param>
 [AttributeUsage( AttributeTargets.Class )]
-public class PawnAttribute : Attribute
+public class PawnAttribute( string prefabPath ) : Attribute
 {
-	public string PrefabPath { get; private set; }
-
-	/// <param name="prefabPath">The path to the prefab that contains the derived pawn component.</param>
-	public PawnAttribute( string prefabPath )
-	{
-		PrefabPath = prefabPath;
-	}
+	public string PrefabPath { get; private set; } = prefabPath;
 }
